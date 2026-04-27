108891916ee24efa64de52baaee38dfea80506eb
yagrado <student.bg@youngsvilleacademy.org>
2026-04-27 08:29:08 -0400

    Create header-injection.js

---
+++ header-injection.js
@@ -0,0 +1,8 @@
+ let elem = document.createElement("div");
+elem.innerHTML = "<link rel='stylesheet' href='https://yagrado.github.io/web-dev/header.css'>"
+document.body.prepend(elem);
+
+fetch("https://yagrado.github.io/web-dev/header.html")
+.then((output)=> output.text())
+.then((text)=> {elem.innerHTML =  elem.innerHTML + text;})
+.catch((e) => console.error(e));
