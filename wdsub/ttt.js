ec544d5b15e8fec825b29b51fa48aeb6e42fdaa9
yagrado <student.bg@youngsvilleacademy.org>
2026-05-20 09:07:33 -0400

    Create ttt.js

---
+++ wdsub/ttt.js
@@ -0,0 +1,84 @@
+let winText = document.querySelector("#end-of-game p");
+let squareArr = document.querySelectorAll("div.ttt-square")
+let EOGDiv = document.getElementById("end-of-game");
+let currentPlayer = "X";
+
+for (const element of squareArr) {
+  element.addEventListener("click",(event)=>markSymbol(event));
+}
+
+
+function markSymbol(event){
+  let clickedSquare = event.target;
+  
+  if (clickedSquare.innerText == ""){
+    clickedSquare.innerText = currentPlayer;
+   if (checkWinner()== false) checkDraw();
+    changePlayer();
+  }
+}
+   function checkWinner(){
+     let IsGameOver = false;
+     //check for three in a row
+     for (let i=0; i<3; i++){
+       if (squareArr[3 * i].innerText == currentPlayer && squareArr[ 3 * i + 1].innerText == currentPlayer && squareArr[3 * i+2].innerText == currentPlayer) isGameOver=true;
+       
+      
+     }
+     
+     //check for three in a column
+     
+     for (let i=0; i<3; i++){
+       if (squareArr[i].innerText == currentPlayer && squareArr[i + 3].innerText == currentPlayer && squareArr[i+6].innerText == currentPlayer) isGameOver=true;
+     }
+     
+     
+     //check for three in a diag
+     if (squareArr[0].innerText == currentPlayer && squareArr[4].innerText == currentPlayer && squareArr[8].innerText == currentPlayer) isGameOver=true;
+     
+     else if (squareArr[2].innerText == currentPlayer && squareArr[4].innerText == currentPlayer &&  squareArr[6].innerText == currentPlayer) isGameOver=true;
+     
+     //if game is over, display end-of-game div
+     if (isGameOver)showWinner();
+       return isGameOver;
+       
+     }
+   
+
+     function checkDraw(){
+       let isDraw = true;
+       
+       for (const elem squareArr){
+         if (elem.innerText == "") isDraw = false;
+         
+       }
+       
+       if(isDraw) showDraw();
+     }
+     
+
+   function showWinner(){
+     EOGDiv.style.display = "block";
+     winText.innerText = "🎉" + currentPlayer + " Player Wins!🎉"
+   } 
+
+
+function showDraw(){
+  EOGDiv.style.display = "block";
+     winText.innerText = "Draw!"
+}
+
+function changePlayer(){
+  currentPlayer = ((currentPlayer = "X") ? "O" : "X");
+  }
+
+
+function resetGame (){
+  //Hide the end-of-game div
+  EOGDiv.style.display = "none"
+  
+  //Clear the board
+  for (const elem of squareArr){
+    elem.innerText = "";
+  }
+}
